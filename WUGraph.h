38a13eae96724d8c77e2f69f916faf7afe262286
kyle chou <672116640@qq.com>
2017-12-30 11:43:26 +0800

    吃饭去了
    啦啦啦

--- WUGraph.h
+++ WUGraph.h
@@ -1,45 +1,46 @@
 #pragma 
+#include<iostream>
+using namespace std;
 const int maxWeight = INT_MAX;
 const int DefaultSize = 100;
+const int MaxVertices = 10000;
 enum KindOfStatus { Active, Empty, Deleted };
 const int DeafultVertices = 30;			//代表最大顶点数 =n
-template<class T,class E>
 struct Vertex
 {
 	int key;
-	T data;
-	Edge<T, E> *adj;
-	Vertex<T, E> *prior,*next;
+	int data;
+	Edge *adj;
+	Vertex *rLink,*lLink;
 };
-template<class T, class E>
 struct  Edge
 {
 	int dest;            //边的另一个顶点位置
-	E cost;              //边上的权值
-	Edge<T, E> * Llink,Rlink;   //下一条边链指针
-	Edge<T, E> * partner;//伙伴指针
+	int cost;              //边上的权值
+	int key;             //边的关键码
+	Edge  *lLink,*rLink;   //下一条边链指针
+	Edge * partner;//伙伴指针
 	Edge() { cost = maxWeight; }
-	Edge(int num, E weight) : dest(num), cost(weight), Rlink(NULL), Llink(NULL) {}
-	bool operator != (Edge<T, E>& R)const
+	Edge(int num, int weight) : dest(num), cost(weight), Rlink(NULL), Llink(NULL) {}
+	bool operator != (Edge& R)const
 	{
 		return (dest != R.dest) ? true : false;
 	}
 
 };
-template<class T, class E>
 class Graphlnk
 {
 public:
 	Graphlnk(int sz = MaxVertices);
 	~Graphlnk();
-	T getValue(int i)					//取位置为i的顶点中的值
+	int getValue(int i)					//取位置为i的顶点中的值
 	{
 		return (i >= 0 && i< numVertices) ? NodeTable[i].nameRouter : 0;
 	}
-	E getWeight(int v1, int v2);			//返回边(v1,v2)的权值
-	bool insertVertex(const Vertex<T, E> vertex);     
+	int getWeight(int v1, int v2);			//返回边(v1,v2)的权值
+	bool insertVertex(const Vertex vertex);     
 	bool removeVertex(int v);
-	bool insertEdge(int v1, int v2, const Edge<T, E> edge);
+	bool insertEdge(int v1, int v2, const Edge edge);
 	bool removeEdge(int v1, int v2);
 	int NumberOfVertices()
 	{
@@ -52,11 +53,13 @@ public:
 				return i;
 		return -1;
 	}
-	void cruskal(int v);
+	void kruskal(int v);
 	
 private:
-	HashTable<Vertex,int> NodeHashtable;
-	HashTable<Edge, int> EdgeHashtable;
+	HashTable  NodeHashtable;
+	HashTable  EdgeHashtable;
+	VertexDblList NodeTable;
+	EdgeDblList EdgeTable;
 	int maxVertices;
 	int numEdges;
 	int numVertices;
