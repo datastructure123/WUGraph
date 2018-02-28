dfc7b2caa989e41ced5f2411cc247cfdb1f94cc6
ZCHOU <34033305+SonGoKu21@users.noreply.github.com>
2018-02-28 16:19:28 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,40 @@
 #include<iostream>
+#include"WUGraph.h"
 void main()
 {
-	return;
+	char x;
+	string Start;
+	string End;
+	Graphlnk Railway;
+
+	Railway.initiate();
+	while (1) {
+		cout << "================旅游导航系统系统=================" << endl
+			<< "==           1.旅游城市信息                      ==" << endl
+			<< "==           2.旅游路线规划                      ==" << endl
+			<< "==           3.退出界面                          ==" << endl;
+		
+		cin >> x;
+		switch (x)
+		{
+		case'1':
+         cout << "城市：" << "数量"<<Railway.vertexCount<<endl;
+		Railway.getVertices();
+			c = getchar();
+			break;
+		case'2':
+			system("cls");
+			cout << "=============旅游路线规划==============" << endl
+				<< "请输入始发点和目的地-->" << endl;
+			cin >> Start >> End;
+			system("cls");
+			cout << "=============旅游路线规划==============" << endl
+				<< "==           最短路径                ==" << endl
+				<< "==           最少花费                ==" << endl;
+			break;
+		
+		case'3':exit(0); break;
+		}
+	}
+	 }
 }
